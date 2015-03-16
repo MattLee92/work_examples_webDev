d2b07656e7168281f8f41e02a5a1e549663b2922
Matthew Lee <matthew.lee3@griffithuni.edu.au>
2015-03-16 12:00:33 +0000

    index.php edited online with Bitbucket

--- Lab2/index.php
+++ Lab2/index.php
@@ -8,7 +8,7 @@
         array('date'=> '12/12/12','message' =>'Hello5','image' => 'images/Lighthouse.jpg'),
         array('date'=> '12/12/12','message' =>'Hello6','image' => 'images/Tulips.jpg'),
         array('date'=> '12/12/12','message' =>'Hello7','image' => 'images/Penguins.jpg'),
-        array('date'=> '12/12/12','message' =>'Hello8','image' => 'images/Desery.jpg'),
+        array('date'=> '12/12/12','message' =>'Hello8','image' => 'images/Desert.jpg'),
         array('date'=> '12/12/12','message' =>'Hello9','image' => 'images/Koala.jpg'),
         array('date'=> '12/12/12','message' =>'Hello10','image' => 'images/Jellyfish.jpg')
         );
